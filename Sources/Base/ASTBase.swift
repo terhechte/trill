fea53987db83d733d5c2de411139c6ae0bbb23d2
Samuel Giddins <segiddins@segiddins.me>
2017-02-26 08:00:12 -0800

    [SourceLocation] Add file to description

--- Sources/Base/ASTBase.swift
+++ Sources/Base/ASTBase.swift
@@ -55,7 +55,7 @@ struct SourceLocation: CustomStringConvertible {
     self.charOffset = charOffset
   }
   var description: String {
-    return "<line: \(line), col: \(column)>"
+    return "<file: \(file ?? "<none>") line: \(line), col: \(column)>"
   }
   static let zero = SourceLocation(line: 0, column: 0)
 }
